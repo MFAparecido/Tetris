fbf5ba2dec03edff5dfdeeefdccaf96e701a6def
MFAparecido <mateus.fernandes060801@gmail.com>
2019-10-09 11:30:57 -0300

    formatos tetris

--- tetris.h
+++ tetris.h
@@ -23,6 +23,34 @@
 #define TECLA_A 65
 #define TECLA_d 100
 #define TECLA_D 68
+//Orientações para as peças
+#define ORIENTACAO_UP 1
+#define ORIENTACAO_DOWN 2
+#define ORIENTACAO_LEFT 3
+#define ORIENTACAO_RIGHT 4
+//Criando os tipos de peças
+#define TIPO_L 1
+#define TIPO_L_R 2 //L reverso
+#define TIPO_T 3
+#define TIPO_Z 4
+#define TIPO_Z_R 5 //Z reverso
+#define TIPO_O 6   //Quadrado
+#define TIPO_I 7
+
+//Configurações de layout
+#define PIXEL 219
+#define EMPTY 32
+
+//Estrutura padrão de componentes
+typedef struct{
+    int i;  //posição nas linhas da matriz
+    int j;  //posição nas colunas da matriz
+    int orientacao; //orientação da peça
+    int tipo;   //o tipo de peça (7 possiveis)
+    int width;  //largura 
+    int height; //altura
+}Bloco;
+
 /*
     Inicializa a matriz principal com 'espaços vazios'
 */
